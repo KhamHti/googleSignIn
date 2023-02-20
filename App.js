7035794c03570cb90548ce1b5ac2d024f5699507
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-02-20 13:42:34 +0630

    first commit
    Sign in with google using react native expo and firebase.

--- App.js
+++ App.js
@@ -1,10 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  GoogleSignin.configure({
+    webClientId:
+      "445448734454-pbodh903ous0eihni72hfk00bsit8qfk.apps.googleusercontent.com",
+  });
+
+  // Handle user state changes
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  const onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({
+      showPlayServicesUpdateDialog: true,
+    });
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    const user_signin = auth().signInWithCredential(googleCredential);
+    user_signin;
+    // .then((user) => {
+    //   console.log(user);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+  };
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <GoogleSigninButton
+          style={{ width: 300, height: 70, marginTop: 300 }}
+          onPress={onGoogleButtonPress}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{user.displayName}</Text>
+      <Image
+        source={{ uri: user.photoURL }}
+        style={{ width: 200, height: 200, margin: 50, borderRadius: 100 }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +78,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+npm install --save @react-native-firebase/app
+npx expo install expo-dev-client
+
+eas build --profile development --platform android
+
+<!-- eas build --profile development-simulator --platform ios -->
+
+npx expo install @react-native-google-signin/google-signin
+npm i @react-native-firebase/auth
